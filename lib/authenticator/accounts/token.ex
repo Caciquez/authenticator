0806d914ca15f703bf19361b01741bf52730b211
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2022-09-14 09:58:48 -0300

    fix: remove unused tests

--- lib/authenticator/accounts/token.ex
+++ lib/authenticator/accounts/token.ex
@@ -2,11 +2,13 @@ defmodule Authenticator.Accounts.Token do
   @salt "1Z73WxH/cDS96wsHXXI8QVAFOy5tg/APqIufGTO8nO2cTn/Mtp7zCnrx+0fSVY1/"
   @expiry 86400
 
+  alias Phoenix.Token
+
   def sign(conn, data) do
-    Phoenix.Token.sign(conn, @salt, data)
+    Token.sign(conn, @salt, data)
   end
 
   def verify(conn, token, opts \\ []) do
-    Phoenix.Token.verify(conn, @salt, token, max_age: Keyword.get(opts, :max_age, @expiry))
+    Token.verify(conn, @salt, token, max_age: Keyword.get(opts, :max_age, @expiry))
   end
 end
--- test/authenticator_web/live/user_live_test.exs
+++ test/authenticator_web/live/user_live_test.exs
@@ -4,14 +4,6 @@ defmodule AuthenticatorWeb.UserLiveTest do
   import Phoenix.LiveViewTest
   import Authenticator.AccountsFixtures
 
-  @create_attrs %{email: "some email", login_token: "some login_token", password: "some password"}
-  @update_attrs %{
-    email: "some updated email",
-    login_token: "some updated login_token",
-    password: "some updated password"
-  }
-  @invalid_attrs %{email: nil, login_token: nil, password: nil}
-
   defp create_user(_) do
     user = user_fixture()
     %{user: user}
@@ -25,50 +17,6 @@ defmodule AuthenticatorWeb.UserLiveTest do
 
       assert html =~ "Listing Users"
       assert html =~ user.email
-    end
-
-    test "saves new user", %{conn: conn} do
-      {:ok, index_live, _html} = live(conn, Routes.user_index_path(conn, :index))
-
-      assert index_live |> element("a", "New User") |> render_click() =~
-               "New User"
-
-      assert_patch(index_live, Routes.user_index_path(conn, :new))
-
-      assert index_live
-             |> form("#user-form", user: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      {:ok, _, html} =
-        index_live
-        |> form("#user-form", user: @create_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.user_index_path(conn, :index))
-
-      assert html =~ "User created successfully"
-      assert html =~ "some email"
-    end
-
-    test "updates user in listing", %{conn: conn, user: user} do
-      {:ok, index_live, _html} = live(conn, Routes.user_index_path(conn, :index))
-
-      assert index_live |> element("#user-#{user.id} a", "Edit") |> render_click() =~
-               "Edit User"
-
-      assert_patch(index_live, Routes.user_index_path(conn, :edit, user))
-
-      assert index_live
-             |> form("#user-form", user: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      {:ok, _, html} =
-        index_live
-        |> form("#user-form", user: @update_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.user_index_path(conn, :index))
-
-      assert html =~ "User updated successfully"
-      assert html =~ "some updated email"
     end
 
     test "deletes user in listing", %{conn: conn, user: user} do
@@ -87,28 +35,6 @@ defmodule AuthenticatorWeb.UserLiveTest do
 
       assert html =~ "Show User"
       assert html =~ user.email
-    end
-
-    test "updates user within modal", %{conn: conn, user: user} do
-      {:ok, show_live, _html} = live(conn, Routes.user_show_path(conn, :show, user))
-
-      assert show_live |> element("a", "Edit") |> render_click() =~
-               "Edit User"
-
-      assert_patch(show_live, Routes.user_show_path(conn, :edit, user))
-
-      assert show_live
-             |> form("#user-form", user: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      {:ok, _, html} =
-        show_live
-        |> form("#user-form", user: @update_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.user_show_path(conn, :show, user))
-
-      assert html =~ "User updated successfully"
-      assert html =~ "some updated email"
     end
   end
 end
